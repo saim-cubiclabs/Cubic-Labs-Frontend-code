2bea6c8e975c771f7c99af897b0a0100d3fb2282
saim-cubiclabs <saim@cubiclabs.net>
2025-09-15 15:37:42 +0300

    added btn fix of ws

--- FrontEnd Codes/javascript/whatsapp.js
+++ FrontEnd Codes/javascript/whatsapp.js
@@ -17,6 +17,7 @@ document.addEventListener("DOMContentLoaded", () => {
     font-size:16px;
     box-shadow:0 2px 6px rgba(0,0,0,0.2);
     z-index:1000;
+    hover:opacity:90
   `;
   document.body.appendChild(btn);
 });
